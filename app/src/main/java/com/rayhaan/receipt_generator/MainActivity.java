27b3c93619684c8b2f2ba044b771d0bd4c3fbd31
Ray <rayhaan.bhikha@sky.uk>
2018-09-22 22:58:36 +0100

    total row styling touch ups

--- app/src/main/java/com/rayhaan/receipt_generator/MainActivity.java
+++ app/src/main/java/com/rayhaan/receipt_generator/MainActivity.java
@@ -34,14 +34,11 @@ public class MainActivity extends AppCompatActivity {
         ListView mListView = (ListView) findViewById(R.id.invoice_items);
         invoiceList = new ArrayList<>();
 
-        Item item1 = new Item("£5.00", "Gold dress");
-        Item item2 = new Item("£45.00", "Blue dress with Lining");
-        Item item3 = new Item("£53.00", "Pink dress repair");
-        Item item4 = new Item("£70.00", "silver dress with embroidery");
-        invoiceList.add(item1);
-        invoiceList.add(item2);
-        invoiceList.add(item3);
-        invoiceList.add(item4);
+        invoiceList.add(new Item("£5.00", "Gold dress"));
+        invoiceList.add(new Item("£45.30", "Blue dress with Lining"));
+        invoiceList.add(new Item("£53.07", "Pink dress repair"));
+        invoiceList.add(new Item("£70.00", "silver dress with embroidery"));
+
 
         calculateTotal();
         adapter = new CustomListAdapter(this, R.layout.row_item, invoiceList);
