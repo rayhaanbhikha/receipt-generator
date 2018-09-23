8e7e2c096862b8c8bd7da070518849035c03bf11
Ray <rayhaan.bhikha@sky.uk>
2018-09-23 15:50:30 +0100

    all rows are now symmetrical

--- app/src/main/java/com/rayhaan/receipt_generator/MainActivity.java
+++ app/src/main/java/com/rayhaan/receipt_generator/MainActivity.java
@@ -47,6 +47,18 @@ public class MainActivity extends AppCompatActivity {
         invoiceList.add(new Item("£45.30", "Blue dress with Lining"));
         invoiceList.add(new Item("£53.07", "Pink dress repair"));
         invoiceList.add(new Item("£70.00", "silver dress with embroidery"));
+        invoiceList.add(new Item("£5.00", "Gold dress"));
+        invoiceList.add(new Item("£45.30", "Blue dress with Lining"));
+        invoiceList.add(new Item("£53.07", "Pink dress repair"));
+        invoiceList.add(new Item("£70.00", "silver dress with embroidery"));
+        invoiceList.add(new Item("£5.00", "Gold dress"));
+        invoiceList.add(new Item("£45.30", "Blue dress with Lining"));
+        invoiceList.add(new Item("£53.07", "Pink dress repair"));
+        invoiceList.add(new Item("£70.00", "silver dress with embroidery"));
+        invoiceList.add(new Item("£5.00", "Gold dress"));
+        invoiceList.add(new Item("£45.30", "Blue dress with Lining"));
+        invoiceList.add(new Item("£53.07", "Pink dress repair"));
+        invoiceList.add(new Item("£70.00", "silver dress with embroidery"));
 
 
 //        calculateTotal();
